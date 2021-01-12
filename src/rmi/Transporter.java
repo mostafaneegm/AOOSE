9ea820f37d6df2cc10368a6ac4955542c998f692
mostafaneegm <64106403+mostafaneegm@users.noreply.github.com>
2021-01-12 18:35:09 +0200

    TCMS

--- src/rmi/Transporter.java
+++ src/rmi/Transporter.java
@@ -4,11 +4,76 @@
  * and open the template in the editor.
  */
 package rmi;
+import java.rmi.RemoteException;
+
 
 /**
  *
  * @author hp
  */
-public class Transporter {
+public class Transporter extends user {
+    int licensenumber;
+    
+    public Transporter () throws RemoteException{}
+    public Transporter(int licensenumber) throws RemoteException{
+        this.licensenumber = licensenumber;
+    }
+    public Transporter(int licensenumber, int ID, String name, String email, String number) throws RemoteException{
+     super(ID,name,email,number);
+             this.licensenumber = licensenumber;             
+    }
+
+    public int getLicensenumber() {
+        return licensenumber;
+    }
+
+    public void setLicensenumber(int licensenumber) {
+        this.licensenumber = licensenumber;
+    }
+    public int getID() throws RemoteException {​​
+return ID;
+}​​
+
+
+
+public void setID(int ID) throws RemoteException {​​
+this.ID = ID;
+}​​
+
+
+
+public String getName()throws RemoteException {​​
+return name;
+}​​
+
+
+
+public void setName(String name) throws RemoteException {​​
+this.name = name;
+}​​
+
+
+
+public String getEmail() throws RemoteException {​​
+return email;
+}​​
+
+
+
+public void setEmail(String email) throws RemoteException {​​
+this.email = email;
+}​​
+
+
+
+public String getNumber() throws RemoteException {​​
+return number;
+}​​
+
+
+
+public void setNumber(String number)throws RemoteException {​​
+this.number = number;
+}​​
     
 }
